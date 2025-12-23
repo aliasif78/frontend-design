8dd918a66d3b434119a497e1c223317a66d8d4c2
aliasif78 <aliasif1171@gmail.com>
2025-12-23 23:37:10 +0500

    NJ: Made responsive

--- app/nike-jordan/page.tsx
+++ app/nike-jordan/page.tsx
@@ -8,35 +8,39 @@ import shoe from "./assets/shoe.webp";
 
 const page = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-between overflow-hidden text-nowrap sm:bg-green-300 md:bg-blue-300 lg:bg-red-300 xl:bg-red-500">
-      {/* Titles */}
-      <div className="font-inter flex flex-col text-center lg:mt-[6rem] xl:mt-[4rem] 2xl:mt-0">
-        <h2 className="text-[110px] font-extrabold tracking-[-10px] text-transparent opacity-30 [-webkit-text-stroke:1px_black] [paint-order:stroke] lg:text-[145px] xl:text-[180px] 2xl:text-[210px]">
-          THE NEW 2023
-        </h2>
+    <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden text-nowrap">
+      <div className="flex h-fit w-fit flex-col items-center justify-center">
+        {/* Titles */}
+        <div className="font-inter -mb-[7vh] flex flex-col text-center sm:-mb-[9vh] md:-mb-[10vh] 2xl:-mb-[15vh]">
+          <h2 className="xs:text-[60px] text-5xl font-extrabold tracking-[-2px] text-transparent opacity-30 [-webkit-text-stroke:1px_black] [paint-order:stroke] sm:text-[75px] md:text-8xl lg:text-9xl 2xl:text-[160px]">
+            THE NEW 2023
+          </h2>
 
-        <h1 className="z-10 -mt-[8rem] [background-image:linear-gradient(to_right,#4280CF_0%,#3975C0_8%,#0C315F_45%,#0A2444_59%,#03234B_82%,#4280CF_100%)] bg-clip-text text-[130px] font-extrabold tracking-[-10px] text-transparent lg:-mt-[11rem] lg:text-[170px] xl:-mt-[14rem] xl:text-[220px] 2xl:text-[250px]">
-          AIR JORDAN
-        </h1>
-      </div>
+          <h1 className="xs:text-[80px] xs:-mt-[4rem] z-10 -mt-[3rem] [background-image:linear-gradient(to_right,#4280CF_0%,#3975C0_8%,#0C315F_45%,#0A2444_59%,#03234B_82%,#4280CF_100%)] bg-clip-text text-[60px] font-extrabold tracking-[-6px] text-transparent sm:-mt-[3rem] sm:text-[100px] sm:tracking-[-10px] md:-mt-[5rem] md:text-9xl lg:text-[165px] 2xl:-mt-[7rem] 2xl:text-[200px]">
+            AIR JORDAN
+          </h1>
+        </div>
 
-      {/* Shoe Image */}
-      <div className="absolute top-1/2 z-50 w-[900px] max-w-none origin-top -translate-y-1/2 scale-[0.9] sm:scale-[0.85] md:scale-[0.9] lg:scale-100 xl:scale-[1.2] 2xl:scale-[1.3]">
-        <Image
-          src={shoe}
-          alt="shoe"
-          className="h-auto w-full object-contain"
-          priority={true}
-        />
-      </div>
+        {/* Shoe Image */}
+        <div className="z-50 w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%]">
+          <Image
+            src={shoe}
+            alt="shoe"
+            className="h-auto w-full object-contain"
+            priority={true}
+          />
+        </div>
 
-      {/* Secondary Text */}
-      <div className="font-inter z-50 flex flex-col gap-4 pb-[2rem] text-center">
-        <h3 className="text-5xl font-extrabold tracking-[-2px]">Just do it</h3>
+        {/* Secondary Text */}
+        <div className="font-inter z-50 flex w-full flex-col items-center justify-center gap-4 pb-[2rem] text-center">
+          <h3 className="text-3xl font-extrabold tracking-[-2px] md:text-5xl">
+            Just do it
+          </h3>
 
-        <p className="text-lg text-[#959799]">
-          Captures messages that stands for more than just sport.
-        </p>
+          <p className="w-[80%] text-center text-wrap text-[#959799] md:text-lg">
+            Captures messages that stands for more than just sport.
+          </p>
+        </div>
       </div>
     </div>
   );
